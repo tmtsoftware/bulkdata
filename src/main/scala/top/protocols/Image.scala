1de708bf03f6d94a2c7202fbb55372b76635c556
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-06-24 18:14:48 +0530

    make all streams finite to see how termination works

--- src/main/scala/top/protocols/Image.scala
+++ src/main/scala/top/protocols/Image.scala
@@ -17,6 +17,8 @@ object Image {
     .scan(1)((acc, _) => acc + 1)
     .map(x => Image(x.toString))
 
+  val ten = stream.take(10)
+
   val single = Source.single(Image(""))
   val lazyEmpty = Source.lazyEmpty[Image]
 
--- src/main/scala/top/protocols/Main.scala
+++ src/main/scala/top/protocols/Main.scala
@@ -10,12 +10,12 @@ object Main extends App {
 
   args match {
     case Array("bidi", "server", host, Int(port)) => new Server(host, port, BidiProtocol).run()
-    case Array("ss", "server", host, Int(port))   => new Server(host, port, new ServerSentProtocol(Image.stream)).run()
+    case Array("ss", "server", host, Int(port))   => new Server(host, port, new ServerSentProtocol(Image.ten)).run()
     case Array("sr", "server", host, Int(port))   => new Server(host, port, ServerReceivedProtocol).run()
 
-    case Array("bidi", "client", host, Int(port)) => new Client(host, port, new ClientProtocol(Image.stream)).run()
+    case Array("bidi", "client", host, Int(port)) => new Client(host, port, new ClientProtocol(Image.ten)).run()
     case Array("ss", "client", host, Int(port))   => new Client(host, port, new ClientProtocol(Image.single)).run()
-    case Array("sr", "client", host, Int(port))   => new Client(host, port, new ClientProtocol(Image.stream.take(10))).run()
+    case Array("sr", "client", host, Int(port))   => new Client(host, port, new ClientProtocol(Image.ten)).run()
 
     case _ => println("Use `host port protocol mode` e.g. 'localhost, 6001, bidi, server'")
   }
