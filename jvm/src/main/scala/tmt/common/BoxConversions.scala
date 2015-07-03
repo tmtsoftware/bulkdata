c51d13a1319ad476dd5326fcb25fbedc4e2d55d8
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-07-03 19:02:00 +0530

    Use SynchronousFileSource and BinaryMessage.Stream

--- jvm/src/main/scala/tmt/common/BoxConversions.scala
+++ jvm/src/main/scala/tmt/common/BoxConversions.scala
@@ -1,30 +1,9 @@
 package tmt.common
 
-import java.io.{ByteArrayInputStream, File}
-import java.nio.file.Files
-import javax.activation.MimetypesFileTypeMap
-import javax.imageio.ImageIO
-
 import akka.util.ByteString
-import boopickle.{Unpickle, Pickle}
+import boopickle.{Pickle, Unpickle}
 
 object BoxConversions {
   def toByteString(box: Box) = ByteString(Pickle.intoBytes(box))
   def fromByteString(byteString: ByteString) = Unpickle[Box].fromBytes(byteString.toByteBuffer)
-}
-
-object ImageConversions {
-  def fromFile(file: File) = {
-    val bytes = Files.readAllBytes(file.toPath)
-    Image(
-      "",
-      "image/jpeg",
-      Config.imageWidth,
-      Config.imageHeight,
-      bytes
-    )
-  }
-
-  def toByteString(image: Image) = ByteString(image.bytes)
-  def fromByteString(byteString: ByteString) = Unpickle[Image].fromBytes(byteString.toByteBuffer)
 }
--- jvm/src/main/scala/tmt/dsl/ImageService.scala
+++ jvm/src/main/scala/tmt/dsl/ImageService.scala
@@ -2,14 +2,14 @@ package tmt.dsl
 
 import akka.http.scaladsl.model.ws.BinaryMessage
 import akka.stream.Materializer
+import akka.stream.io.SynchronousFileSource
 import akka.stream.scaladsl.Source
 import tmt.common._
 
 class ImageService(implicit mat: Materializer) {
 
   def send = Source(() => Producer.files)
-    .map(ImageConversions.fromFile)
-    .map(ImageConversions.toByteString)
-    .map(BinaryMessage.Strict)
+    .map(file => SynchronousFileSource(file))
+    .map(BinaryMessage.apply)
 
 }
