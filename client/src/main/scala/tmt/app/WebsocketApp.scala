e618bc50a760273fcbe01ae28441efdbccfb9a9b
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-09-28 00:22:50 +0530

    reuse refactoring of ui

--- client/src/main/scala/tmt/app/WebsocketApp.scala
+++ client/src/main/scala/tmt/app/WebsocketApp.scala
@@ -2,6 +2,7 @@ package tmt.app
 
 import org.scalajs.dom
 import org.scalajs.dom._
+import org.scalajs.dom.html.Select
 import tmt.common.models.PerSecMetric
 import tmt.common.{ImageRateControls, CanvasControls, PerSecControls}
 import tmt.images.ImageRendering
@@ -14,28 +15,22 @@ object WebsocketApp extends JSApp {
 
   @JSExport
   override def main() = {
-    perSecMetrics()
-    showImages()
+    render(PerSecControls.select) { socket =>
+      MetricsRendering.render[PerSecMetric](socket, PerSecControls.span)
+    }
+    render(CanvasControls.select) { socket =>
+      ImageRendering.drain(socket)
+    }
     changeImageRate()
   }
 
-  def perSecMetrics(): Unit = {
+  def render(select: Select)(block: WebSocket => Unit): Unit = {
     var socket: WebSocket = null
-    PerSecControls.select.onchange = { e: Event =>
+    select.onchange = { e: Event =>
       if(socket != null) socket.close()
-      socket = new WebSocket(PerSecControls.select.value)
+      socket = new WebSocket(select.value)
       socket.binaryType = "arraybuffer"
-      MetricsRendering.render[PerSecMetric](socket, PerSecControls.span)
-    }
-  }
-
-  def showImages(): Unit = {
-    var socket: WebSocket = null
-    CanvasControls.select.onchange = { e: Event =>
-      if(socket != null) socket.close()
-      socket = new WebSocket(CanvasControls.select.value)
-      socket.binaryType = "arraybuffer"
-      ImageRendering.drain(socket)
+      block(socket)
     }
   }
 
