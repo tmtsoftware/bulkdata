ed98bad56390faa0f0d3edfa8c48e70a195929bd
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-10-03 20:23:21 +0530

    Use persistent actor for connection store

--- frontend/app/actors/ConnectionStore.scala
+++ frontend/app/actors/ConnectionStore.scala
@@ -1,28 +1,38 @@
 package actors
 
-import akka.actor.{Actor, Props}
+import akka.actor.Props
 import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.Subscribe
+import akka.persistence.PersistentActor
 import tmt.common.Messages
 import tmt.shared.Topics
 import tmt.shared.models.ConnectionSet
 
-class ConnectionStore extends Actor {
+class ConnectionStore extends PersistentActor {
+
+  def persistenceId = "connection-store"
 
   var connectionSet = ConnectionSet.empty
 
   val mediator = DistributedPubSub(context.system).mediator
 
-
   override def preStart() = {
     mediator !  Subscribe(Topics.Subscription, self)
   }
 
-  def receive = {
-    case Messages.Subscribe(serverName, topic)   => connectionSet = connectionSet.addConnection(serverName, topic)
-    case Messages.Unsubscribe(serverName, topic) => connectionSet = connectionSet.removeConnection(serverName, topic)
-    case ConnectionStore.GetConnections          => sender() ! connectionSet
+  def receiveRecover = {
+    case event: Messages.Subscribe      => addConnection(event)
+    case event: Messages.Unsubscribe    => removeConnection(event)
   }
+
+  def receiveCommand = {
+    case event: Messages.Subscribe      => persistAsync(event)(addConnection)
+    case event: Messages.Unsubscribe    => persistAsync(event)(removeConnection)
+    case ConnectionStore.GetConnections => sender() ! connectionSet
+  }
+
+  def addConnection(event: Messages.Subscribe) = connectionSet = connectionSet.addConnection(event.serverName, event.topic)
+  def removeConnection(event: Messages.Unsubscribe) = connectionSet = connectionSet.removeConnection(event.serverName, event.topic)
 }
 
 object ConnectionStore {
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,15 +26,17 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-cluster" % Versions.Akka,
     "com.typesafe.akka" %% "akka-cluster-tools" % Versions.Akka,
     "com.typesafe.akka" %% "akka-cluster-metrics" % Versions.Akka,
-    "com.typesafe.akka" %% "akka-distributed-data-experimental" % Versions.Akka,
-    "com.typesafe.akka" %% "akka-persistence" % Versions.Akka
+    "com.typesafe.akka" %% "akka-distributed-data-experimental" % Versions.Akka
   )
 
   val frontendLibs = Seq(
     "com.vmunier" %% "play-scalajs-scripts" % "0.3.0",
     "org.webjars" % "jquery" % "1.11.1",
     "com.typesafe.akka" % "akka-slf4j_2.11" % Versions.Akka,
-    "com.lihaoyi" %% "scalatags" % "0.5.2"
+    "com.lihaoyi" %% "scalatags" % "0.5.2",
+    "com.typesafe.akka" %% "akka-persistence" % Versions.Akka,
+    "org.iq80.leveldb" % "leveldb" % "0.7",
+    "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8"
   )
 
   val backendLibs = Seq(
@@ -43,7 +45,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-http-experimental" % Versions.Streams,
     "net.codingwell" %% "scala-guice" % "4.0.0",
 
-  //test
+    //test
     "org.scalatest" %% "scalatest" % "2.2.5" % "test",
     "com.typesafe.akka" %% "akka-stream-testkit-experimental" % Versions.Streams % "test",
     "com.typesafe.akka" %% "akka-http-testkit-experimental" % Versions.Streams % "test"
