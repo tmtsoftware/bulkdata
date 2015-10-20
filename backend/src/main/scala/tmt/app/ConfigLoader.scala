58a2ebad798fdfea764bf401923c71b96ea1df53
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-10-20 15:26:51 +0530

    testing netwrok api

--- backend/src/main/scala/tmt/app/ConfigLoader.scala
+++ backend/src/main/scala/tmt/app/ConfigLoader.scala
@@ -1,11 +1,24 @@
 package tmt.app
 
+import java.net.{NetworkInterface, InetAddress}
+
 import com.typesafe.config._
 import tmt.library.ConfigObjectExtensions.RichConfigObject
+import collection.JavaConverters._
 
 class ConfigLoader {
 
   def load(name: String, env: String) = {
+    println(InetAddress.getLocalHost.getHostName)
+    println(InetAddress.getLocalHost.getHostAddress)
+    println("********************")
+    NetworkInterface.getNetworkInterfaces.asScala.foreach { inter =>
+      println("^^^^^^^^^^^^^^^^^^^^^^", inter.getName)
+      inter.getInetAddresses.asScala.foreach { inet =>
+        println("%%%%%%%%%%%%%%%%%%%%%%", inet.getHostName)
+        println("%%%%%%%%%%%%%%%%%%%%%%", inet.getHostAddress)
+      }
+    }
     val config = parse(env)
     val binding = ConfigFactory.empty().withValue(
       "binding", config.getObject(s"bindings.$name").withPair("name", name)
