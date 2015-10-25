0585bff728fa895fbbb1f776e67032f3e9174d4c
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-10-25 16:56:27 +0530

    refactoring

--- client/src/main/scala/tmt/views/SubscriptionView.scala
+++ client/src/main/scala/tmt/views/SubscriptionView.scala
@@ -29,38 +29,34 @@ class SubscriptionView(dataStore: ViewData)(implicit ec: ExecutionContext) exten
   def viewTitle = h5("Connect")
 
   def viewContent = div(
-    label("Select output server"),
+    label("Output server"),
     makeSelection(dataStore.producers, topicName),
 
-    label("Select input server"),
+    label("Input server"),
     makeSelection(consumers, serverName)
   )
 
   def viewAction = div(
     a(cls := "btn-floating waves-effect waves-light")(
-      onclick := {() => addConnection()},
+      onclick := { () => addConnection() },
       i(cls := "material-icons")("add")
     ),
-
     Rx {
-      table(
-        tbody(
-          connectionSet().connections.toSeq.map { c =>
-            tr(
-              td(c.topic),
-              td("====>"),
-              td(c.server),
-              td(
-                a(cls := "btn-floating waves-effect waves-light red")(
-                  onclick := {() => removeConnection(c)},
-                  i(cls := "material-icons")("remove")
-                )
-              )
-            )
-          }
-        )
-      )
+      val rows = connectionSet().connections.toSeq.map(makeRow)
+      table(tbody(rows))
     }
+  )
+
+  def makeRow(c: Connection) = tr(
+    td(c.topic),
+    td("====>"),
+    td(c.server),
+    td(
+      a(cls := "btn-floating waves-effect waves-light red")(
+        onclick := { () => removeConnection(c) },
+        i(cls := "material-icons")("remove")
+      )
+    )
   )
 
   def addConnection() = {
