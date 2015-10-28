289855367fbb73eac35e92140e6932fcfea0b7b9
Mushtaq Ahmed <mushtaq.a@gmail.com>
2015-10-28 16:47:49 +0530

    test the new image transformation method

--- backend/src/main/scala/tmt/transformations/ImageProcessor.scala
+++ backend/src/main/scala/tmt/transformations/ImageProcessor.scala
@@ -21,12 +21,13 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class ImageProcessor(appSettings: AppSettings) {
+  Loader.load(classOf[opencv_core])
 
   private val imageProcessingEc = ExecutionContext.fromExecutorService(
     Executors.newFixedThreadPool(appSettings.imageProcessingThreadPoolSize)
   )
 
-  def rotate(image: Image) = async {
+  def dd(image: Image) = async {
     val bufferedImage = ImageIO.read(new ByteArrayInputStream(image.bytes))
     val rotatedBufferedImage = Scalr.rotate(bufferedImage, Rotation.FLIP_VERT)
     makeNewImage(image.name, rotatedBufferedImage)
@@ -41,14 +42,30 @@ class ImageProcessor(appSettings: AppSettings) {
     rotatedImage
   }
 
+  def rotate(image: Image) = async {
+    val bytePointer = new BytePointer(ByteBuffer.wrap(image.bytes))
+    val iplImage = opencv_imgcodecs.cvDecodeImage(opencv_core.cvMat(1, image.bytes.length, opencv_core.CV_8UC1, bytePointer))
+    val rotatedImage = rotate1(iplImage, 180)
+    val encodeImage = opencv_imgcodecs.cvEncodeImage(".jpeg", rotatedImage)
+    val array = new Array[Byte](encodeImage.total() * encodeImage.channels())
+    encodeImage.data_ptr().get(array)
+    Image(image.name, array)
+  }(imageProcessingEc)
 
+  def rotate1(src: IplImage, angle: Int): IplImage = {
+    val newImg = AbstractIplImage.create(src.height, src.width, src.depth, src.nChannels)
+    opencv_core.cvTranspose(src, newImg)
+    opencv_core.cvFlip(newImg, newImg, angle)
+    newImg
+  }
+  
 }
 
 object A {
   def dd = {
     Loader.load(classOf[opencv_core])
     val bytes = Files.readAllBytes(new File("/usr/local/data/tmt/frames/input/image-10003.jpeg").toPath)
-    val bytePointer = new BytePointer(bytes: _*)
+    val bytePointer = new BytePointer(ByteBuffer.wrap(bytes))
     val image = opencv_imgcodecs.cvDecodeImage(opencv_core.cvMat(1, bytes.length, opencv_core.CV_8UC1, bytePointer))
 
     val encodeImage: CvMat = opencv_imgcodecs.cvEncodeImage(".jpeg", rotate(image, 90))
@@ -62,6 +79,5 @@ object A {
     opencv_core.cvTranspose(src, newImg)
     opencv_core.cvFlip(newImg, newImg, angle)
     newImg
-    //    opencv_core.cvSave("/usr/local/data/tmt/frames/output/000.jpeg", newImg)
   }
 }
